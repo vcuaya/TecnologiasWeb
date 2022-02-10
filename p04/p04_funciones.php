b35b4832a92d3436316d9d02d6928a9e2ab846f8
vcuaya <vcuaya@outlook.com>
2022-02-10 02:50:23 -0600

    Práctica 4: Ejercicio 2

--- p04/p04_funciones.php
+++ p04/p04_funciones.php
@@ -2,12 +2,26 @@
 function multiplo($numero)
 {
     if ($numero % 5 == 0)
-        echo 'El n&uacute;mero ' . $numero . ' es m&uacute;ltiplo de 5';
+        echo 'El n&uacute;mero ' . $numero . ' es m&uacute;ltiplo de 5<br/>';
     else
-        echo 'El n&uacute;mero ' . $numero . ' no es m&uacute;ltiplo de 5';
-    
+        echo 'El n&uacute;mero ' . $numero . ' no es m&uacute;ltiplo de 5<br/>';
+
     if ($numero % 7 == 0)
-        echo 'El n&uacute;mero ' . $numero . ' es m&uacute;ltiplo de 7';
+        echo 'El n&uacute;mero ' . $numero . ' es m&uacute;ltiplo de 7<br/>';
     else
-        echo 'El n&uacute;mero ' . $numero . ' no es m&uacute;ltiplo de 7';
+        echo 'El n&uacute;mero ' . $numero . ' no es m&uacute;ltiplo de 7<br/>';
+}
+
+function random()
+{
+    $i = 0;
+    $matriz;
+    do {
+        $matriz[$i][0] = rand(100, 999);
+        $matriz[$i][1] = rand(100, 999);
+        $matriz[$i][2] = rand(100, 999);
+        echo $matriz[$i][0] . ', ' . $matriz[$i][1] . ', ' . $matriz[$i][2] . '<br/>';
+        $i++;
+    } while (!($matriz[$i - 1][0] % 2 != 0 && $matriz[$i - 1][1] % 2 == 0 && $matriz[$i - 1][2] % 2 != 0));
+    echo ($i*3).' n&uacute;meros obtenidos en ' . $i . ' iteraciones<br/>';
 }
