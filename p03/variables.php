63c29be1f5b6b6233c2b5997737f46a5a5b63dfe
vcuaya <vcuaya@outlook.com>
2022-02-16 20:24:04 -0600

    Validación de la Práctica 3 utilizando la herramienta de la W3C https://validator.w3.org/

--- p03/variables.php
+++ p03/variables.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Strict//EN'	'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd'>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns='http://www.w3.org/1999/xhtml' xml:lang='es' lang='es'>
-    <head>
-        <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
-        <title>Pr&aacute;ctica 3: Variables</title>
-        <link rel='stylesheet' href='style.css' type='text/css' media='screen' charset='utf-8'/>
-    </head>
-    <body>
+
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <title>Pr&aacute;ctica 3: Variables</title>
+    <link rel='stylesheet' href='style.css' type='text/css' media='screen' charset='utf-8' />
+</head>
+
+<body>
     <?php
     echo "================================================<br>1. Toda variable debe iniciar con el símbolo de moneda seguido de un guión bajo o una letra.<br>Se colocó un espacio para que el interprete no mostrara error por variables no definidas<br>";
     echo "$ _myvar;    // Variable válida<br>";
@@ -17,110 +19,110 @@
     echo "$ house*5;   // Variable inválida, no se puede usar un operador binario como parte de una variable<br>";
 
     echo "<br>================================================<br>2. Proporcionar los valores de $ a, $ b, $ c<br>";
-    $a="ManejadorSQL";
-    $b='MySQL';
-    $c=&$a;
+    $a = "ManejadorSQL";
+    $b = 'MySQL';
+    $c = &$a;
     echo "$a,<br> $b,<br> $c<br>";
     unset($a);
     unset($b);
     unset($c);
 
     echo "En esta sección del código ahora las variables $ b y $ c hacen referencia a la variable $ a es por ello que las tres variales muestran el mismo valor<br>";
-    $a="PHP server";
-    $b=&$a;
+    $a = "PHP server";
+    $b = &$a;
     echo "$a,<br> $b,<br>";
 
     echo "<br>================================================<br>3.Mostrar el contenido de cada variable<br>";
-    $a="PHP5";
-    echo "Valor de a: " . $a . "<br>Tipo de dato ". gettype($a)."<br><br>";
+    $a = "PHP5";
+    echo "Valor de a: " . $a . "<br>Tipo de dato " . gettype($a) . "<br><br>";
 
-    $z[]=&$a;
+    $z[] = &$a;
     echo "Valor de z: ";
     var_dump($z);
-    echo "<br>Tipo de dato ". gettype($z)."<br><br>";
+    echo "<br>Tipo de dato " . gettype($z) . "<br><br>";
 
-    $b="5a version de PHP";
-    echo "Valor de b: " . $b . "<br>Tipo de dato ". gettype($b)."<br><br>";
+    $b = "5a version de PHP";
+    echo "Valor de b: " . $b . "<br>Tipo de dato " . gettype($b) . "<br><br>";
 
     settype($b, "integer");
-    $c=$b*10;
-    echo "Valor de c: " . $c . "<br>Tipo de dato ". gettype($c)."<br><br>";
+    $c = $b * 10;
+    echo "Valor de c: " . $c . "<br>Tipo de dato " . gettype($c) . "<br><br>";
 
-    $a.=$b;
-    echo "Valor de a: " . $a . "<br>Tipo de dato ". gettype($a)."<br><br>";
+    $a .= $b;
+    echo "Valor de a: " . $a . "<br>Tipo de dato " . gettype($a) . "<br><br>";
 
-    $b*=$c;
-    echo "Valor de b: " . $b . "<br>Tipo de dato ". gettype($b)."<br><br>";
+    $b *= $c;
+    echo "Valor de b: " . $b . "<br>Tipo de dato " . gettype($b) . "<br><br>";
 
-    $z[0]="MySQL";
+    $z[0] = "MySQL";
     echo "Valor de z: ";
     var_dump($z);
-    echo "<br>Tipo de dato ". gettype($z)."<br><br>";
+    echo "<br>Tipo de dato " . gettype($z) . "<br><br>";
     unset($a);
     unset($b);
     unset($c);
     unset($z);
 
     echo "<br>================================================<br>4. Uso de Globals<br>";
-    $a="PHP5";
+    $a = "PHP5";
     function funcion1()
     {
         global $a;
-        echo "Valor de a: " . $a . "<br>Tipo de dato ". gettype($a)."<br><br>";
+        echo "Valor de a: " . $a . "<br>Tipo de dato " . gettype($a) . "<br><br>";
     }
     funcion1();
 
-    $z[]=&$a;
+    $z[] = &$a;
     function funcion2()
     {
         global $z;
         echo "Valor de z: ";
         var_dump($z);
-        echo "<br>Tipo de dato ". gettype($z)."<br><br>";
+        echo "<br>Tipo de dato " . gettype($z) . "<br><br>";
     }
     funcion2();
 
-    $b="5a version de PHP";
+    $b = "5a version de PHP";
     function funcion3()
     {
         global $b;
-        echo "Valor de b: " . $b . "<br>Tipo de dato ". gettype($b)."<br><br>";
+        echo "Valor de b: " . $b . "<br>Tipo de dato " . gettype($b) . "<br><br>";
     }
     funcion3();
 
     settype($b, "integer");
-    $c=$b*10;
+    $c = $b * 10;
     function funcion4()
     {
         global $c;
-        echo "Valor de c: " . $c . "<br>Tipo de dato ". gettype($c)."<br><br>";
+        echo "Valor de c: " . $c . "<br>Tipo de dato " . gettype($c) . "<br><br>";
     }
     funcion4();
 
-    $a.=$b;
+    $a .= $b;
     function funcion5()
     {
         global $a;
-        echo "Valor de a: " . $a . "<br>Tipo de dato ". gettype($a)."<br><br>";
+        echo "Valor de a: " . $a . "<br>Tipo de dato " . gettype($a) . "<br><br>";
     }
     funcion5();
 
     //settype($a, "integer");
-    $b*=$c;
+    $b *= $c;
     function funcion6()
     {
         global $b;
-        echo "Valor de b: " . $b . "<br>Tipo de dato ". gettype($b)."<br><br>";
+        echo "Valor de b: " . $b . "<br>Tipo de dato " . gettype($b) . "<br><br>";
     }
     funcion6();
 
-    $z[0]="MySQL";
+    $z[0] = "MySQL";
     function funcion7()
     {
         global $z;
         echo "Valor de z: ";
         var_dump($z);
-        echo "<br>Tipo de dato ". gettype($z)."<br><br>";
+        echo "<br>Tipo de dato " . gettype($z) . "<br><br>";
     }
     funcion7();
 
@@ -131,16 +133,16 @@
 
     echo "<br>================================================<br>5. Mostrar el valor de las variables<br>";
     $a = "7 personas";
-    $b = (integer) $a;
+    $b = (int) $a;
     $a = "9E3";
-    $c = (double) $a;
+    $c = (float) $a;
 
-    echo "Los valores son:<br>" . $a ."<br>". $b ."<br>" . $c."<br><br>";
+    echo "Los valores son:<br>" . $a . "<br>" . $b . "<br>" . $c . "<br><br>";
 
     echo "<br>================================================<br>6. Comprobar valor booleano<br>";
     echo "Impresión con var_dump<br>";
     $a = "0";
-    if(is_bool($a))
+    if (is_bool($a))
         echo "El valor de 'a' es booleano ";
     else
         echo "El valor de 'a' no es booleano ";
@@ -148,7 +150,7 @@
     echo "<br>";
 
     $b = "TRUE";
-    if(is_bool($b))
+    if (is_bool($b))
         echo "El valor de 'b' es booleano ";
     else
         echo "El valor de 'b' no es booleano ";
@@ -156,31 +158,31 @@
     echo "<br>";
 
     $c = FALSE;
-    if(is_bool($c))
+    if (is_bool($c))
         echo "El valor de 'c' es booleano ";
     else
         echo "El valor de 'c' no es booleano ";
     var_dump($c);
     echo "<br>";
 
-    $d = ($a OR $b);
-    if(is_bool($d))
+    $d = ($a or $b);
+    if (is_bool($d))
         echo "El valor de 'd' es booleano ";
     else
         echo "El valor de 'd' no es booleano ";
     var_dump($d);
     echo "<br>";
 
-    $e = ($a AND $c);
-    if(is_bool($e))
+    $e = ($a and $c);
+    if (is_bool($e))
         echo "El valor de 'e' es booleano ";
     else
         echo "El valor de 'e' no es booleano ";
     var_dump($e);
     echo "<br>";
 
-    $f = ($a XOR $b);
-    if(is_bool($f))
+    $f = ($a xor $b);
+    if (is_bool($f))
         echo "El valor de 'f' es booleano ";
     else
         echo "El valor de 'f' no es booleano ";
@@ -189,25 +191,29 @@
 
     echo "Impresión con echo<br>";
     echo "El valor de 'c' es ";
-    echo var_export($c)."<br>";
+    echo var_export($c) . "<br>";
     echo "El valor de 'e' es ";
-    echo var_export($e)."<br>";
+    echo var_export($e) . "<br>";
 
     echo "<br>================================================<br>7. Variable predefinida $ _SERVER<br>";
-    echo "La versión de Apache y PHP es ".$_SERVER['SERVER_SOFTWARE']."<br>";
-    echo "El nombre del sistema operativo es ".$_SERVER["HTTP_USER_AGENT"]."<br>";
+    echo "La versión de Apache y PHP es " . $_SERVER['SERVER_SOFTWARE'] . "<br>";
+    echo "El nombre del sistema operativo es " . $_SERVER["HTTP_USER_AGENT"] . "<br>";
 
-    $idioma=substr($_SERVER['HTTP_ACCEPT_LANGUAGE'],0,2);
-    if($idioma == "es"){
+    $idioma = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    if ($idioma == "es") {
         echo "El idioma del navegador del cliente es español";
-    }
-    elseif($idioma == "en"){
+    } elseif ($idioma == "en") {
         echo "El idioma del navegador del cliente es inglés";
-    }
-    else{
+    } else {
         echo "El idioma del navegador del cliente es otro idioma diferente a español e inglés";
     }
 
     ?>
-    </body>
+
+    <p>
+        <a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
+    </p>
+
+</body>
+
 </html>
