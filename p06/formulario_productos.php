f980d46ea060d6b0a9a836db814ad311693d2d1e
vcuaya <vcuaya@outlook.com>
2022-02-20 03:04:47 -0600

    Práctica 6: Creación de formulario y validación del formulario desde PHP

---
+++ p06/formulario_productos.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <title>Agregar Productos</title>
+    <link rel='stylesheet' href='style.css' type='text/css' media='screen' charset='utf-8' />
+</head>
+
+<body>
+    
+    <form action="set_producto_v2.php" method="post">
+        <fieldset>
+            <legend>Agregar producto</legend>
+            <fieldset>
+                <legend>N&uacute;mero de parte y Nombre del producto</legend>
+                <ul>
+                    <li><label for="formSKU">SKU</label><input type="text" name="sku" placeholder="DCPT700W" maxlength="25" autofocus></li>
+                    <li><label for="formNombre">Nombre</label><input type="text" name="nombre" size="50" placeholder="Multifuncional Brother DCP-T700W" maxlength="100"></li>
+                </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Marca y Modelo del producto</legend>
+                <ul>
+                    <li><label for="formMarca">Marca</label><input type="text" name="marca" placeholder="Brother" maxlength="25"></li>
+                    <li><label for="formModelo">Modelo</label><input type="text" name="modelo" placeholder="DCP-T700W" maxlength="25"></li>
+                </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Cantidad y Precio del producto</legend>
+                <ul>
+                    <li><label for="formUnidades">Cantidad</label><input type="text" name="unidades"></li>
+                    <li><label for="formPrecio">Precio</label><input type="text" name="precio"></li>
+                </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Detalles y ruta de la Imagen del producto</legend>
+                <ul>
+                    <li><label for="formDetalles">Detalles</label><input type="text" name="detalles" size="50" placeholder="Color, Inyecci&oacute;n, Inal&aacute;mbrico, Print/Scan/Copy" maxlength="250"></li>
+                    <li><label for="formImagen">Imagen</label><input type="text" name="imagen" placeholder="img/imagen.png" maxlength="100"></li>
+                </ul>
+            </fieldset>
+            <input type="submit" value="Guardar">
+            <input type="reset">
+        </fieldset>
+    </form>
+</body>
+
+</html>
---
+++ p06/set_producto_v2.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns='http://www.w3.org/1999/xhtml' xml:lang='en' lang='en'>
+
+<?php
+if (empty($_POST['sku'])) {
+    echo '<script type="text/javascript">alert("El SKU no puede quedar vacío");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (empty($_POST['nombre'])) {
+    echo '<script type="text/javascript">alert("El NOMBRE no puede quedar vacío");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (empty($_POST['marca'])) {
+    echo '<script type="text/javascript">alert("La MARCA no puede quedar vacía");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (empty($_POST['modelo'])) {
+    echo '<script type="text/javascript">alert("El MODELO no puede quedar vacío");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (empty($_POST['unidades'])) {
+    echo '<script type="text/javascript">alert("La CANTIDAD debe ser un valor entero mayor a 0");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (!ctype_digit($_POST['unidades'])) {
+    echo '<script type="text/javascript">alert("La CANTIDAD debe ser un valor entero mayor a 0");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (empty($_POST['precio'])) {
+    echo '<script type="text/javascript">alert("El PRECIO debe ser un valor mayor a 0");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (!is_numeric($_POST['precio'])) {
+    echo '<script type="text/javascript">alert("El PRECIO debe ser un valor mayor a 0");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif ($_POST['precio'] < 0) {
+    echo '<script type="text/javascript">alert("El PRECIO debe ser un valor mayor a 0");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (empty($_POST['detalles'])) {
+    echo '<script type="text/javascript">alert("El valor DETALLES no puede quedar vacío");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} elseif (empty($_POST['imagen'])) {
+    echo '<script type="text/javascript">alert("El valor IMAGEN no puede quedar vacío");</script>';
+    echo '<script type="text/javascript">window.history.back(-1)</script>';
+} else {
+    @$link = new mysqli('localhost', 'root', 'localhost', 'marketzone');
+
+    if ($link->connect_errno) {
+        die('Falló la conexión: ' . $link->connect_error . '<br/>');
+    }
+
+    $sql = "INSERT INTO productos VALUES (null, '{$_POST['sku']}', '{$_POST['nombre']}', '{$_POST['marca']}', '{$_POST['modelo']}', {$_POST['precio']}, '{$_POST['detalles']}', {$_POST['unidades']}, '{$_POST['imagen']}')";
+    if ($link->query($sql)) {
+        $id = $link->insert_id;
+    }
+
+    $link->close();
+}
+?>
+
+<head>
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <title>Resumen de Producto Ingresado</title>
+    <link rel='stylesheet' href='style.css' type='text/css' media='screen' charset='utf-8' />
+</head>
+
+<body>
+    
+    <?php if (isset($id)) : ?>
+        <h1>Producto Registrado</h1>
+        <fieldset>
+            <legend>Resumen de producto</legend>
+            <fieldset>
+                <legend>N&uacute;mero de parte y Nombre del producto</legend>
+                <ul>
+                    <li><strong>ID:</strong><?php global $id;
+                                            echo $id ?></li>
+                    <li><strong>SKU:</strong><?php echo $_POST['sku'] ?></li>
+                    <li><strong>Nombre:</strong><?php echo $_POST['nombre'] ?></li>
+                </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Marca y Modelo del producto</legend>
+                <ul>
+                    <li><strong>Marca:</strong><?php echo $_POST['marca'] ?></li>
+                    <li><strong>Modelo:</strong><?php echo $_POST['modelo'] ?></li>
+                </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Cantidad y Precio del producto</legend>
+                <ul>
+                    <li><strong>Cantidad:</strong><?php echo $_POST['unidades'] ?></li>
+                    <li><strong>Precio</strong><?php echo $_POST['precio'] ?></li>
+                </ul>
+            </fieldset>
+            <fieldset>
+                <legend>Detalles e Imagen del producto</legend>
+                <ul>
+                    <li><strong>Detalles:</strong><?php echo $_POST['detalles'] ?></li>
+                    <li><strong>Imagen:</strong><?php echo $_POST['imagen']; ?></li>
+                </ul>
+            </fieldset>
+        </fieldset>
+    <?php else : ?>
+        <script type="text/javascript">
+            alert("El Producto no pudo ser ingresado =(");
+        </script>';
+    <?php endif; ?>
+</body>
+
+</html>
